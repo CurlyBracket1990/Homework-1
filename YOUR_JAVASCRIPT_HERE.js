8f7e2cdaf956a7eb330956780f3cc00b99b0e1f1
CurlyBracket1990 <39054285+CurlyBracket1990@users.noreply.github.com>
2019-01-12 12:42:11 +0100

    Equip weapon function
    Wrote the function that equips the first item in the heros inventory to the weapon slot + wrote some comments in the js file

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -1,3 +1,4 @@
+//Hero object with basic keys and values
 const hero = {
     name: "HenkyTanky",
     heroic: true,
@@ -9,19 +10,29 @@ const hero = {
     }
 };
 
+//New weapon called dagger
 const dagger = {
     type: "dagger",
     damage: 2
 };
 
+//Resting function that resets hero's hp
 function rest(currentHero) {
     currentHero.health = 10;
     return currentHero;
 };
 
+//Function that makes the hero pick up the clicked item
 function pickUpItem(currentHero, pickedUpWeapon) {
     const lastPlace = currentHero.inventory.length;
     currentHero.inventory[lastPlace] = pickedUpWeapon;
     return currentHero;
 };
 
+//Function that equips the first item in the heros inventory
+function equipWeapon (currentHero) {
+    if(!currentHero.inventory.length == 0){
+    currentHero.weapon = currentHero.inventory[0];
+    }
+    return currentHero;
+};
