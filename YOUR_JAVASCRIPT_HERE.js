c0b524bf7624d8416a87394f124a3187051794bf
CurlyBracket1990 <39054285+CurlyBracket1990@users.noreply.github.com>
2019-01-12 11:48:32 +0100

    Wrote logic for picking an item up and placing it in the inventory

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -4,12 +4,24 @@ const hero = {
     inventory: [],
     health: 10,
     weapon : {
-        type: "Chaos",
+        type: "knife",
         damage: 2
     }
+};
+
+const dagger = {
+    type: "dagger",
+    damage: 2
 };
 
 function rest(currentHero) {
     currentHero.health = 10;
     return currentHero;
 };
+
+function pickUpItem(currentHero, pickedUpWeapon) {
+    const lastPlace = currentHero.inventory.length;
+    currentHero.inventory[lastPlace] = pickedUpWeapon;
+    return currentHero;
+};
+
